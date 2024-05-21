2bd3e754bac48d6c7e1643bc6092c63c145f1233
trungchanh2002 <trungchanh.kda@gmail.com>
2024-05-21 16:42:00 +0700

    update : login with passengers

--- passengersService/routes/passengerRoutes.js
+++ passengersService/routes/passengerRoutes.js
@@ -2,13 +2,13 @@ const { Router } = require("express");
 const router = Router();
 const passengerController = require("../controllers/passengerController");
 
-// http://localhost:3000/api/v1/passengers/getAllPassengers
+// http://localhost:3001/api/v1/passengers/getAllPassengers
 router.get("/getAllPassengers", passengerController.getAllPassengers);
-// http://localhost:3000/api/v1/passengers/getPassengerById/664c48537766b84903d71c38
+// http://localhost:3001/api/v1/passengers/getPassengerById/664c48537766b84903d71c38
 router.get("/getPassengerById/:id", passengerController.getPassengerById);
-// http://localhost:3000/api/v1/passengers/login
+// http://localhost:3001/api/v1/passengers/login
 router.post("/login", passengerController.login);
-// http://localhost:3000/api/v1/passengers/signup
+// http://localhost:3001/api/v1/passengers/signup
 router.post("/signup", passengerController.signup);
 //API to update passenger details
 module.exports = router;
