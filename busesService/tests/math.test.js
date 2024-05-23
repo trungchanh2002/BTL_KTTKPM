0659371b7b148adb6286631cd78ed42c368bdf5d
trungchanh2002 <trungchanh.kda@gmail.com>
2024-05-23 22:45:05 +0700

    update : gitlab ci/cd

--- busesService/tests/math.test.js
+++
@@ -1,9 +0,0 @@
-describe("Math", () => {
-  describe("other test", () => {
-    it("should return true if a number is greater than zero", () => {
-      // Giả sử bạn muốn kiểm tra nếu một số là lớn hơn 0
-      const number = 5;
-      expect(number > 0).toBe(true); // Kiểm tra xem số 5 có lớn hơn 0 không
-    });
-  });
-});
